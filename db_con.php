5f6db482f92054ff0e775825a65849c4ea49139e
Minseo Jang <gs24091@gs.hs.kr>
2025-08-21 16:25:44 +0900

    Add files via upload

---
+++ db_con.php
@@ -0,0 +1,10 @@
+<?php
+$conn = mysqli_connect(
+    "localhost",
+    "DBUSERNAME",
+    "DBUSERPASSWORD",
+    "DBNAME");
+
+mysqli_set_charset($conn,'utf8');
+
+?>
---
+++ ui_footer.php
@@ -0,0 +1,13 @@
+
+<!-- <div class="row mt-4">
+    <div class="col-12 bg-info">
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdxNfldgtUQL7uo-QyU2wX8jwN8h76LggITRoGeb0jZLK0KbQ/viewform?usp=sf_link" style="color:white; font-size:14px;">
+            UI/UX 개선을 위한 설문조사가 진행중이에요 😊
+        </a>
+    </div>
+</div> -->
+
+<footer class="py-3 my-4 mt-4">
+    <p class="text-center text-secondary" style="font-weight: bold;">© 2024 <a href="./index.php" class="text-secondary" style="text-decoration: none;"><span class="text-primary">HW.</span>minseo.net</a></p>
+    <!-- <p class="text-center text-secondary">CONTACT : 장민서(gs24091@gs.hs.kr)</p> -->
+</footer>
